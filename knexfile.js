3fed1645454c578ab16ee5da7c932848a80c653f
Bruno Cruz De Oliveira <62567655+bruno2406@users.noreply.github.com>
2025-06-09 09:22:48 -0300

    Frontend

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+const { db } = require('./.env')
+
+module.exports = {
+	client: 'pg',
+	connection: db,
+	pool: {
+		min: 2,
+		max: 20
+	},
+	migrations: {
+		tableName: 'knex_migrations'
+	}
+};
